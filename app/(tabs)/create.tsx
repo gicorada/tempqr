4b8442ffa00359e182fa7d3970050324fad751b3
Giacomo Radaelli <gradaellig@protonmail.com>
2024-06-28 15:14:29 +0200

    temp fix for scanner, started implementing qr creation

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,9 +1,42 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { supabase } from '../../utils/supabase';
+import QRCode from 'react-native-qrcode-svg'; 
 
 export default function Tab() {
+  const [data, setData] = useState<{ uuid: string } | null>(null);
+  const [qrCreated, setQrCreated] = useState(false);
+  const [qrValue, setQRValue] = useState< string | null>(null); 
+
+  const createQRCode = async () => {
+    try {
+      const { data: supabaseData, error } = await supabase.rpc('add_qr');
+  
+      if (error) {
+        throw error;
+      } else {
+        console.log('Data from Supabase:', supabaseData);
+        alert(`QR code created with uuid ${supabaseData}`);
+
+        setQrCreated(true);
+        setQRValue(supabaseData);
+      }
+    } catch (error: any) {
+      console.error('Error fetching data from Supabase:', error.message);
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Tab Settings</Text>
+      <Button title='Create a new qr code' onPress={createQRCode} />
+      {qrCreated && qrValue ? (
+        <QRCode 
+          value={qrValue}
+          size={200} 
+          color="black"
+          backgroundColor="white"
+        />
+      ) : null}
     </View>
   );
 }
--- components/Scan.js
+++ components/Scan.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from "react-native";
 import { CameraView, Camera } from "expo-camera";
+import { validate as validateUUID } from 'uuid';
 import { supabase } from '../utils/supabase';
 
 export default function Scan() {
@@ -25,34 +26,44 @@ export default function Scan() {
 	  }
 
 	const handleBarCodeScanned = async ({ type, data }) => {
-		console.log("scanned");
+		console.log("scanned but not checked yet");
 		setScanned(true);
+		
+		// Check if the scanned data is a valid UUID
+		if (!validateUUID(data)) {
+			alert('Invalid QR Code');
+			console.log("The content of the qr code is not an UUID");
+			return;
+		}
+
 		alert(`Bar code with type ${type} and data ${data} has been scanned!`);
 		
 		try {
-		const { data: supabaseData, error } = await supabase
-			.from('qr')
-			.select('*');
+			const { data: supabaseData, error } = await supabase
+				.from('qr')
+				.select('created_at, text')
+				.eq('id', data)
+				.limit(1);
 
-		if (error) {
-			throw error;
-		} else {
-			console.log(supabaseData);
-		}
+			if (error) {
+				throw error;
+			} else {
+				console.log("the qr code \"", supabaseData[0].text, "\" was created on ", supabaseData[0].created_at);
+				
+			}
 		} catch (error) {
-		console.error('Error fetching data from Supabase:', error.message);
-		// Gestione dell'errore: mostra un messaggio all'utente o gestisci diversamente
+			console.error('Error fetching data from Supabase:', error.message);
 		}
 	};
 
 	return (
-		<View style={styles.container}>
+		<View>
 		  <CameraView
 			onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
 			barcodeScannerSettings={{
 			  barcodeTypes: ["qr", "pdf417"],
 			}}
-			style={StyleSheet.absoluteFillObject}
+			style={{width: 500, height: 100, flex: 1}} // Very temporary fix, i hate this fix
 		  />
 		  {scanned && (
 			<Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
