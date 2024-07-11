68f2f24c780481902e554183f2ab35b471228322
Giacomo Radaelli <gradaellig@protonmail.com>
2024-07-11 16:28:47 +0200

    Added personalized text in qr code, changed style in creation page

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { supabase } from '../../utils/supabase';
-import QRCode from 'react-native-qrcode-svg';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { supabase } from "../../utils/supabase";
+import QRCode from "react-native-qrcode-svg";
 
 // import hook
 import { useTranslation } from "react-i18next";
 
 // Custom styling
-import { Buttons } from '../../constants/Buttons';
+import { Buttons } from "../../constants/Buttons";
+import { Inputs } from "@/constants/Inputs";
+import { Texts } from "@/constants/Texts";
 
 export default function Tab() {
   const [data, setData] = useState<{ uuid: string } | null>(null);
   const [qrCreated, setQrCreated] = useState(false);
-  const [qrValue, setQRValue] = useState< string | null>(null); 
+  const [qrValue, setQRValue] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [qrText, setQrText] = useState("");
   const { t } = useTranslation();
 
   const createQRCode = async () => {
@@ -22,41 +31,58 @@ export default function Tab() {
     setIsLoading(true);
 
     try {
-      const { data: supabaseData, error } = await supabase.rpc('add_qr');
-  
+      const { data: supabaseData, error } = await supabase.rpc("add_qr", {
+        user_text: qrText,
+      });
+
       if (error) {
         throw error;
       } else {
         setQrCreated(true);
+        setQrText("");
         setQRValue(supabaseData);
       }
     } catch (error: any) {
-      console.error('Error fetching data from Supabase:', error.message);
+      console.error("Error fetching data from Supabase:", error.message);
     } finally {
       setIsLoading(false);
     }
   };
-  
+
   return (
     <View style={styles.container}>
-      
-      {qrCreated && qrValue ? (
-        <QRCode 
-        value={qrValue}
-        size={250} 
-        color="black"
-        backgroundColor="white"
-        />
-      ) : <TouchableOpacity activeOpacity={0.8} onPress={createQRCode} disabled={isLoading} style={Buttons.button}>
-          <Text style={ Buttons.buttonText }>{t('create.newQR')}</Text>
-        </TouchableOpacity>}
+      <View style={[{ position: "absolute", top: 10 }]}>
+        {qrValue ? (
+          <QRCode
+            value={qrValue}
+            size={250}
+            color="black"
+            backgroundColor="white"
+          />
+        ) : (
+          <Text style={Texts.text}>TEST: create a qr code</Text>
+        )}
+      </View>
 
-      {qrCreated && qrValue ? (
-        <TouchableOpacity activeOpacity={0.8} onPress={createQRCode} disabled={isLoading} style={[Buttons.button, { position: 'absolute', bottom:20 }]}>
-          <Text style={ Buttons.buttonText }>{t('create.anotherQR')}</Text>
+      <View style={[{ position: "absolute", bottom: 10 }]}>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={createQRCode}
+          disabled={isLoading}
+          style={Buttons.button}
+        >
+          <Text style={Buttons.buttonText}>
+            {isLoading ? t("create.loading") : t("create.newQR")}
+          </Text>
         </TouchableOpacity>
-      ) : null }
 
+        <TextInput
+          value={qrText}
+          onChangeText={(text) => setQrText(text)}
+          style={Inputs.input}
+          placeholder={t("create.enterText")}
+        />
+      </View>
     </View>
   );
 }
@@ -64,7 +90,7 @@ export default function Tab() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- components/Scan.js
+++ components/Scan.js
@@ -1,108 +1,135 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Pressable, Modal, Animated, Vibration } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+  Modal,
+  Animated,
+  Vibration,
+} from "react-native";
 import { CameraView, Camera } from "expo-camera";
-import { validate as validateUUID } from 'uuid';
-import { supabase } from '../utils/supabase';
-import ModalComponent from './ModalComponent';
+import { validate as validateUUID } from "uuid";
+import { supabase } from "../utils/supabase";
+import ModalComponent from "./ModalComponent";
 
 // import hook
 import { useTranslation } from "react-i18next";
 
 // Custom styling
-import { Buttons } from '../constants/Buttons';
-
+import { Buttons } from "../constants/Buttons";
 
 export default function Scan() {
-	const [hasPermission, setHasPermission] = useState(null);
-	const [scanned, setScanned] = useState(true);
-	const [data, setData] = useState(null);
-	const [scamModalVisible, setScamModalVisible] = useState(false);
-	const [successModalVisible, setSuccessModalVisible] = useState(false);
-	const [alreadyValidatedModalVisible, setAlreadyValidatedModalVisible] = useState(false);
-	const [notValidModalVisible, setNotValidModalVisible] = useState(false);
-	const [otherOrganizationModalVisible, setOtherOrganizationModalVisible] = useState(false);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(true);
+  const [data, setData] = useState(null);
+  const [scamModalVisible, setScamModalVisible] = useState(false);
+  const [successModalVisible, setSuccessModalVisible] = useState(false);
+  const [alreadyValidatedModalVisible, setAlreadyValidatedModalVisible] =
+    useState(false);
+  const [notValidModalVisible, setNotValidModalVisible] = useState(false);
+  const [otherOrganizationModalVisible, setOtherOrganizationModalVisible] =
+    useState(false);
   const [noInternetModalVisible, setNoInternetModalVisible] = useState(false);
-	const { t } = useTranslation();
+  const { t } = useTranslation();
 
-	const shakeAnimationValue = new Animated.Value(0);
+  const shakeAnimationValue = new Animated.Value(0);
 
-	const SCAM_PATTERN = [5, 50, 5, 100, 5, 50, 5, 100, 5, 50, 500];
+  const SCAM_PATTERN = [5, 50, 5, 100, 5, 50, 5, 100, 5, 50, 500];
 
-	useEffect(() => {
-		const getCameraPermissions = async () => {
-		  const { status } = await Camera.requestCameraPermissionsAsync();
-		  setHasPermission(status === "granted");
-		};
-	
-		getCameraPermissions();
-	}, []);
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
 
-	if (hasPermission === null) {
-		return <Text>{t('scan.cameraPermissionRequest')}</Text>;
-	}
-	if (hasPermission === false) {
-		return <Text>{t('scan.noCameraPermission')}</Text>;
-	}
+    getCameraPermissions();
+  }, []);
 
-	const handleBarCodeScanned = async ({ type, data }) => {
-		Vibration.vibrate(100);
-		setScanned(true);
-		// Check if the scanned data is a valid UUID
-		if (!validateUUID(data)) {
-			setScamModalVisible(true);
-			Vibration.vibrate(SCAM_PATTERN, true);
-			return;
-		}
-				
-		try {
-			const { data: supabaseData, error } = await supabase.rpc('check_qr', {qr_uuid: data})
-			
-			if (error) {
-				throw error;
-			} else {
-				setData(supabaseData);
+  if (hasPermission === null) {
+    return <Text>{t("scan.cameraPermissionRequest")}</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>{t("scan.noCameraPermission")}</Text>;
+  }
 
-				if(supabaseData.status === 'ok') {
-					setSuccessModalVisible(true);
-				} else if(supabaseData.status === 'already_validated') {
-					setAlreadyValidatedModalVisible(true);
-				} else if(supabaseData.status === 'wrong_organization') {
-					setOtherOrganizationModalVisible(true);
-				} else {
-					setNotValidModalVisible(true);
-				}
-			}
-		} catch (error) {
-      if(error.message === 'TypeError: Network request failed') {
+  const handleBarCodeScanned = async ({ type, data }) => {
+    Vibration.vibrate(100);
+    setScanned(true);
+    // Check if the scanned data is a valid UUID
+    if (!validateUUID(data)) {
+      setScamModalVisible(true);
+      Vibration.vibrate(SCAM_PATTERN, true);
+      return;
+    }
+
+    try {
+      const { data: supabaseData, error } = await supabase.rpc("check_qr", {
+        qr_uuid: data,
+      });
+
+      if (error) {
+        throw error;
+      } else {
+        setData(supabaseData.status);
+
+        if (supabaseData.status === "ok") {
+          setSuccessModalVisible(true);
+        } else if (supabaseData.status === "already_validated") {
+          setAlreadyValidatedModalVisible(true);
+        } else if (supabaseData.status === "wrong_organization") {
+          setOtherOrganizationModalVisible(true);
+        } else {
+          setNotValidModalVisible(true);
+        }
+      }
+    } catch (error) {
+      if (error.message === "TypeError: Network request failed") {
         setNoInternetModalVisible(true);
       }
-			console.error('Error fetching data from Supabase:', error.message);
-		}
-	};
+      console.error("Error fetching data from Supabase:", error.message);
+    }
+  };
 
-	return (
-		<View style={styles.container}>
-			<CameraView
-				onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
-				barcodeScannerSettings={{
-					barcodeTypes: ["qr", "pdf417"],
-				}}
-				style={{width: 430, height: 100, flex: 1}} // Very temporary fix, i hate this fix
-			/>
+  return (
+    <View style={styles.container}>
+      <CameraView
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr", "pdf417"],
+        }}
+        style={{ width: 430, height: 100, flex: 1 }} // Very temporary fix, i hate this fix
+      />
 
-			<TouchableOpacity onPress={() => setScanned(false)} activeOpacity={0.8} style={[Buttons.button, { maxHeight: 250 }, scanned ? styles.buttonScanned : styles.buttonNotScanned]}>
-				{ scanned ? <Text style={ Buttons.buttonText }>{t('scan.scanAgain')}</Text> : <Text style={ Buttons.buttonText }>{t('scan.scanning')}.</Text> }
-			</TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setScanned(false)}
+        activeOpacity={0.8}
+        style={[
+          Buttons.button,
+          { maxHeight: 250 },
+          scanned ? styles.buttonScanned : styles.buttonNotScanned,
+        ]}
+      >
+        {scanned ? (
+          <Text style={Buttons.buttonText}>{t("scan.scanAgain")}</Text>
+        ) : (
+          <Text style={Buttons.buttonText}>{t("scan.scanning")}.</Text>
+        )}
+      </TouchableOpacity>
 
-			<View>
+      <View>
         {/* Modal Components */}
         <ModalComponent
           visible={scamModalVisible}
-          onClose={() => { setScamModalVisible(false); Vibration.cancel(); }}
+          onClose={() => {
+            setScamModalVisible(false);
+            Vibration.cancel();
+          }}
           icon="alert-circle"
           animateIcon={true}
-          title={t('scan.modals.scam.title')}
-          text={t('scan.modals.scam.description')}
+          title={t("scan.modals.scam.title")}
+          text={t("scan.modals.scam.description")}
           buttonColor="red"
         />
 
@@ -111,8 +138,8 @@ export default function Scan() {
           onClose={() => setSuccessModalVisible(false)}
           icon="checkmark-circle"
           animateIcon={false}
-          title={t('scan.modals.success.title')}
-          text={t('scan.modals.success.description')}
+          title={t("scan.modals.success.title")}
+          text={t("scan.modals.success.description")}
           buttonColor="green"
         />
 
@@ -121,8 +148,8 @@ export default function Scan() {
           onClose={() => setAlreadyValidatedModalVisible(false)}
           icon="arrow-undo-circle"
           animateIcon={false}
-          title={t('scan.modals.alreadyValidated.title')}
-          text={t('scan.modals.alreadyValidated.description')}
+          title={t("scan.modals.alreadyValidated.title")}
+          text={t("scan.modals.alreadyValidated.description")}
           buttonColor="orange"
         />
 
@@ -131,8 +158,8 @@ export default function Scan() {
           onClose={() => setNotValidModalVisible(false)}
           icon="close-circle"
           animateIcon={false}
-          title={t('scan.modals.error.title')}
-          text={t('scan.modals.error.description')}
+          title={t("scan.modals.error.title")}
+          text={t("scan.modals.error.description")}
           buttonColor="red"
         />
 
@@ -141,8 +168,8 @@ export default function Scan() {
           onClose={() => setOtherOrganizationModalVisible(false)}
           icon="business"
           animateIcon={false}
-          title={t('scan.modals.otherOrganization.title')}
-          text={t('scan.modals.otherOrganization.description')}
+          title={t("scan.modals.otherOrganization.title")}
+          text={t("scan.modals.otherOrganization.description")}
           buttonColor="darkred"
         />
 
@@ -151,13 +178,13 @@ export default function Scan() {
           onClose={() => setNoInternetModalVisible(false)}
           icon="wifi"
           animateIcon={false}
-          title={t('scan.modals.otherOrganization.title')}
-          text={t('scan.modals.otherOrganization.description')}
+          title={t("scan.modals.otherOrganization.title")}
+          text={t("scan.modals.otherOrganization.description")}
           buttonColor="grey"
         />
-			</View>
-		</View>
-	);
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -165,17 +192,17 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
- 	},
+  },
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 22,
-	},
-	buttonScanned: {
-		backgroundColor: 'blue',
-	},
-	buttonNotScanned: {
-		backgroundColor: 'red',
-	},
+  },
+  buttonScanned: {
+    backgroundColor: "blue",
+  },
+  buttonNotScanned: {
+    backgroundColor: "red",
+  },
 });
